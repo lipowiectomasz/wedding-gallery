a90d7c9e5a8fc5d2e96bf8b5e9eb6c059565a3e4
Tomasz Lipowiec <lipowiectomasz@gmail.com>
2026-07-26 00:12:28 +0200

    feat: add appwrite-cli service and database setup script

---
+++ appwrite/config/setup.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+APPWRITE="npx --yes appwrite-cli"
+CLI="$APPWRITE databases"
+DB_ID="wedding_gallery"
+
+$CLI create-collection \
+  --database-id "$DB_ID" \
+  --collection-id profiles \
+  --name profiles \
+  --permissions 'create("users")' 'read("users")'
+
+$CLI create-string-attribute --database-id "$DB_ID" --collection-id profiles --key userId --size 64 --required true
+$CLI create-string-attribute --database-id "$DB_ID" --collection-id profiles --key fullName --size 128 --required true
+$CLI create-string-attribute --database-id "$DB_ID" --collection-id profiles --key deviceId --size 64 --required true
+$CLI create-integer-attribute --database-id "$DB_ID" --collection-id profiles --key photoCount --required true --min 0 --max 20
+
+$CLI create-index --database-id "$DB_ID" --collection-id profiles --key userId_idx --type unique --attributes userId
+
+$CLI create-collection \
+  --database-id "$DB_ID" \
+  --collection-id photos \
+  --name photos \
+  --permissions 'create("users")' 'read("users")'
+
+$CLI create-string-attribute --database-id "$DB_ID" --collection-id photos --key fileId --size 64 --required true
+$CLI create-string-attribute --database-id "$DB_ID" --collection-id photos --key uploaderId --size 64 --required true
+$CLI create-string-attribute --database-id "$DB_ID" --collection-id photos --key uploaderName --size 128 --required true
+$CLI create-string-attribute --database-id "$DB_ID" --collection-id photos --key deviceId --size 64 --required true
+$CLI create-integer-attribute --database-id "$DB_ID" --collection-id photos --key seq --required true --min 1 --max 20
+
+$CLI create-index --database-id "$DB_ID" --collection-id photos --key device_seq_idx --type unique --attributes deviceId seq
+
+$APPWRITE storage create-bucket \
+  --bucket-id event-photos \
+  --name event-photos \
+  --permissions 'create("users")' 'read("users")' \
+  --maximum-file-size 15000000 \
+  --allowed-file-extensions jpg jpeg png heic webp \
+  --compression none \
+  --antivirus false
